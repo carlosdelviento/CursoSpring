7cdbefcb35797fca3b2155e09176d48a6c63519e
Carlos Barbieri <54383068+carlosdelviento@users.noreply.github.com>
2021-03-10 13:12:29 -0300

    Delete HolaMundoSpringData directory

--- HolaMundoSpringData/src/main/java/uy/com/cb/dao/PersonaDao.java
+++
@@ -1,8 +0,0 @@
-package uy.com.cb.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import uy.com.cb.domain.Persona;
-
-public interface PersonaDao extends CrudRepository<Persona, Long> {
-	
-}
