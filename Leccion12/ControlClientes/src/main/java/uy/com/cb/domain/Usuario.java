6064f3dfd6abaa6686f3517009ba6a5372a890d9
Carlos Barbieri <54383068+carlosdelviento@users.noreply.github.com>
2021-03-19 16:51:38 -0300

    fix error table usuario

--- Leccion12/ControlClientes/src/main/java/uy/com/cb/domain/Usuario.java
+++ Leccion12/ControlClientes/src/main/java/uy/com/cb/domain/Usuario.java
@@ -8,13 +8,12 @@ import lombok.Data;
 
 @Entity
 @Data
-@Table(name="rol")
+@Table(name="usuario")
 public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_usuario")
 	private Long idUsuario;
 	
 	@NotEmpty
